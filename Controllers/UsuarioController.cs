4f15ed827c237ac0a7bedd579472232fa7be9e8b
VINICIUS LARANGEIRA GONZALEZ <vinicius.vlg@hotmail.com>
2023-01-09 21:08:22 -0300

    feito

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -5,22 +5,25 @@ using System.Threading.Tasks;
 using CadastroUsuario.Context;
 using CadastroUsuario.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 
 namespace CadastroUsuario.Controllers
 {
     public class UsuarioController: Controller
     {
-
+    
          private readonly UsuarioContext _context;
         
         public UsuarioController(UsuarioContext context)
         {
             _context = context;
         }
+        
         public IActionResult Cadastro()
         {
             return View();
         }
+        
         [HttpPost]
         public IActionResult Criar(Usuario usuario)
         {
@@ -40,18 +43,46 @@ namespace CadastroUsuario.Controllers
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Entrar(Usuario login)
-        {
-         var 
-         if()
-           return RedirectToAction(nameof(Index));
+        { 
+          var loginUser = _context.Usuarios.Where(a => a.Email.Equals(login.Email) && a.Senha.Equals(login.Senha)).FirstOrDefault();
 
-        return View(login.Nome);
+        if(loginUser !=null)
+        {   
+           
+            return View(loginUser);
+        }else
+        {
+           return BadRequest("Usuario ou senha invalidos");
+        }
+       
         }
 
-        public IActionResult RecuperarSenha()
+        public IActionResult Recuperar()
         {
             return View();
         }
-    }
+
+        [HttpPost]
+        public IActionResult RecuperarSenha(Usuario email)
+        {              
+        var loginemail = _context.Usuarios.Where(e => e.Email.Equals(email.Email)).FirstOrDefault();
+
+        if(loginemail !=null)
+        {   
+           
+            return BadRequest("achou");
+        }else
+        {
+           return BadRequest("Usuario ou senha invalidos");
+        }
+    
+
+        }
+           
+        }
 }
+    
+
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -10,7 +10,13 @@ namespace CadastroUsuario.Models
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome do usuário", AllowEmptyStrings = false)]
+        [EmailAddress]
         public string Email { get; set; }
+        
+        [Required(ErrorMessage = "Informe a senha do usuário", AllowEmptyStrings = false)]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Password)]
         public string Senha { get; set; }
 
         //nunca deve apenas implementar verificações no código do cliente - seu código de cliente e ser facilmente ignorado - o servidor deve executar as verificações - seu cliente pode fazê-las também - para economizar uma viagem de ida e volta do servidor e melhorar a experiência do usuário - mas o servidor deve verificar todos os envios
